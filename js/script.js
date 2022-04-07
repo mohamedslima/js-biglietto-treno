4052c537062c357c7b97db5ccae043807cbb6a6c
mohamedslima <hammaslima@gmail.com>
2022-04-07 15:42:28 +0200

    added data user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 
+// DATI IN POsSESSO
+
+let price = 0.21 
+
+// ELABORO DATI UTENTE
+
+const distanceKm = prompt("Quanti kilometri vuoi percorrere?")
+const yourAge = prompt("Quanti anni hai?")
+console.log(distanceKm, yourAge);
+
+
